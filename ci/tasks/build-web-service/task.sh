39bf9490d5fe76366815245606c002973b2a5009
omearaj <jamie4626@yahoo.com>
2017-06-09 18:24:20 -0600

    another issue

--- ci/tasks/build-web-service/task.sh
+++ ci/tasks/build-web-service/task.sh
@@ -4,10 +4,10 @@ set -e
 
 #VERSION=`cat version/number`
 
-pushd web-service
+pushd web-ui
 #  ./gradlew -PversionNumber=$VERSION clean assemble
   ./gradlew clean assemble
 popd
 
 #cp project/build/libs/$ARTIFACT_ID-$VERSION.jar build-output/.
-cp web-service/build/libs/$ARTIFACT_ID.jar build-output/.
+cp web-ui/build/libs/$ARTIFACT_ID.jar build-output/.
